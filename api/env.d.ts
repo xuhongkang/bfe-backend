b559a8a8ea557897ade10733e9cd115419b735d8
Kane Xu <xu.hong@northeastern.edu>
2023-06-17 00:26:49 -0400

    Delete api directory

--- api/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            NEO4J_URI: string;
-            NEO4J_USER: string;
-            NEO4J_PASSWORD: string;
-        }
-    }
-}
