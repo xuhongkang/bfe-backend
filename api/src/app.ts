e22af2baa710c78eda5d96f8f4bf0be4ba348425
Kane Xu <kanexu.work@gmail.com>
2023-06-19 21:33:16 -0400

    8080 -> 3000

--- api/src/app.ts
+++ api/src/app.ts
@@ -44,7 +44,7 @@ const main = async () => {
         json(),
         expressMiddleware(server)
         );
-    app.listen(8080, () => {
+    app.listen(3000, () => {
         console.log('Server running at http://localhost:8080/graphql');
     });
 };
